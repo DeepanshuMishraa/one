a53af307d687b3f444c0d2a5cbdbb8735518a1ab
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-05-17 22:10:51 +0530

    og and info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,56 @@ const ws = Work_Sans({
 export const metadata: Metadata = {
   title: "One",
   description: "Chat with your calendar | One",
+  openGraph: {
+    title: "One",
+    description: "Chat with your calendar | One",
+    url: "https://one.deepanshumishra.xyz",
+    siteName: "One",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "https://one.deepanshumishra.xyz/og.png",
+        width: 800,
+        height: 600,
+        alt: "One",
+      }
+    ],
+  },
+  authors: [{ name: "Deepanshu Mishra", url: "https://deepanshumishra.xyz" }, { name: "One", url: "https://one.deepanshumishra.xyz" }],
+  keywords: [
+    "One",
+    "Talk to calendar",
+    "One AI Calendar",
+    "AI calendar",
+    "chat with calendar",
+    "ycombinator",
+    "deepanshu mishra",
+    "nextjs",
+    "zero.email",
+    "zero",
+    "0.email",
+    "google calendar"
+  ],
+
+  twitter: {
+    card: "summary_large_image",
+    title: "One",
+    description: "Chat with your calendar | One",
+    images: ["https://one.deepanshumishra.xyz/og.png"],
+    creator: "@deepanshuDipxsy",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
