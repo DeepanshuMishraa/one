f3ecc6eb416029ad2f99319daf39385ef2b17607
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-05-23 23:26:01 +0530

    exposed env fix

--- apps/web/lib/ai.ts
+++ apps/web/lib/ai.ts
@@ -2,6 +2,5 @@ import OpenAI from "openai";
 
 export const openai = new OpenAI({
   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
-  apiKey: "AIzaSyCmVlGTc4-cu1HlMMTfWYoCtKzHCLyHgSE",
-  dangerouslyAllowBrowser: true
+  apiKey: process.env.GEMINI_API_KEY,
 })
