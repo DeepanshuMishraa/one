25a345ec9511635089b04520d897383bf959273d
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-06-04 01:10:09 +0530

    unlocks

--- apps/tanstack/app/lib/auth.client.ts
+++
@@ -1,4 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-export const { useSession, signIn, signOut } = createAuthClient({
-  baseURL: "http://localhost:8787",
-})
--- apps/tanstack/app/lib/mem0.ts
+++
@@ -1,3 +0,0 @@
-import MemoryClient from 'mem0ai';
-
-export const client = new MemoryClient({ apiKey: process.env.MEM0_API_KEY as string });
